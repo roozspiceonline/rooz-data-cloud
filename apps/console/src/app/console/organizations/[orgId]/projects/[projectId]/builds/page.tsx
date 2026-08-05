a8eb90b9e0a708bb63774faffbf6e66a1f5c7946
Roozspice <roozspice@gmail.com>
2026-08-06 03:51:50 +0530

    feat(phase1a): add apps/console/src/app/console/organizations/[orgId]/projects/[projectId]/builds/page.tsx

---
+++ apps/console/src/app/console/organizations/[orgId]/projects/[projectId]/builds/page.tsx
@@ -0,0 +1,5 @@
+import { SectionPlaceholder } from "@/components/section-placeholder";
+
+export default function Page() {
+  return <SectionPlaceholder title="Builds" description="Build metadata will dispatch jobs to an isolated worker; this API process will never build user code." status="foundation" />;
+}
